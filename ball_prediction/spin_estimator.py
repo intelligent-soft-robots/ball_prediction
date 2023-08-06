51f40b86546fc796a90e5e322fbd35e152c5dedf
Alexander Dittrich <alexander@dittrich.xyz>
2023-08-07 00:50:59 +0200

    updated spin estimator

--- ball_prediction/spin_estimator.py
+++ ball_prediction/spin_estimator.py
@@ -18,6 +18,8 @@ from numpy import (
     polynomial,
     sqrt,
     zeros,
+    sin,
+    cos,
 )
 from numpy.linalg import norm
 from scipy.signal import find_peaks
@@ -108,7 +110,7 @@ def velocity_regression(
     polynomial_degree: int = POLYNOMIAL_DEGREE,
 ) -> ndarray:
     velocities = empty_like(positions)
-    
+
     for axis in range(3):
         position_polynomial = polynomial.Polynomial.fit(
             time_stamps, positions[:, axis], deg=polynomial_degree
@@ -116,7 +118,6 @@ def velocity_regression(
 
         velocity_polynomial = position_polynomial.deriv()
 
-    
         velocities[:, axis] = velocity_polynomial(time_stamps)
 
     return velocities
@@ -237,41 +238,134 @@ def linear_table_contact(velocity_before_bounce):
 
 
 def lineare_racket_contact(
-    velocity_before_bounce: Sequence[float], racket_orientation: Sequence[float]
+    velocity_before_bounce: Sequence[float], robot_joint_angles_rad: Sequence[float]
 ):
-    # assume no friction, point contact, no restitution, ball hits racket on flat surface,
-    # surface is even
-    velocity_after_bounce = velocity_before_bounce
+    racket_normal = compute_racket_orientation(joint_angles_rad=robot_joint_angles_rad)
+    velocity_after_bounce = calculate_rebound_velocity(
+        velocity_before_bounce, racket_normal
+    )
 
     return velocity_after_bounce
 
 
-def compute_racket_orientation(joint_angles_rad):
-    quaternion = empty(4)
-    normal_vector = empty(3)
+def compute_racket_orientation(
+    joint_angles_rad,
+):
+    theta_1, theta_2, theta_3, theta_4 = joint_angles_rad
 
-    return quaternion, normal_vector
+    def rotation_matrix_x(theta):
+        c = cos(theta)
+        s = sin(theta)
+        return array([[1, 0, 0, 0], [0, c, -s, 0], [0, s, c, 0], [0, 0, 0, 1]])
+
+    def rotation_matrix_y(theta):
+        c = cos(theta)
+        s = sin(theta)
+        return array([[c, 0, s, 0], [0, 1, 0, 0], [-s, 0, c, 0], [0, 0, 0, 1]])
+
+    def rotation_matrix_z(theta):
+        c = cos(theta)
+        s = sin(theta)
+        return array([[c, -s, 0, 0], [s, c, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+
+    # Assuming robot is set up not perfectly within tennicam coordinates
+    theta_x_error = 0.0
+    theta_y_error = 0.0
+    theta_z_error = 0.0
+
+    T_X = rotation_matrix_x(theta_x_error)
+    T_Y = rotation_matrix_y(theta_y_error)
+    T_Z = rotation_matrix_z(theta_z_error)
+
+    T_COMP = T_Z @ T_Y @ T_X
+
+    racket_normal = [0.0, 1.0, 0.0, 1.0]
+
+    T_10 = rotation_matrix_z(theta_1)
+    T_21 = rotation_matrix_y(theta_2)
+    T_43 = rotation_matrix_y(theta_3)
+    T_65 = rotation_matrix_z(theta_4)
+
+    T_60 = T_65 @ T_43 @ T_21 @ T_10
+
+    trans_racket_normal = T_60 @ T_COMP @ racket_normal
+
+    return trans_racket_normal
 
 
-def calculate_rebound_velocity(V_initial, R_orient, C_normal):
+def filter_rebounds(
+    contact_dict: dict,
+    time_stamps: Optional[Sequence[float]] = None,
+    index_threshold: int = 10,
+    time_threshold: float = 0.1,
+    time_filter_stamp_range = 5,
+):
+    filtered_contact_dict = contact_dict.copy()
+    
+    print(f"Contact dict: {filtered_contact_dict}")
+    
+    # remove rebound from index if there is less then threshold measurement samples before and after
+    keys_to_remove = []
+    
+    for key, value in filtered_contact_dict.items():
+        print(key)
+        # Samples below threshold are removed. At the border of the ball tracking system often noisy
+        # samples can be found.
+        if key < index_threshold:
+            keys_to_remove.append(key)
+            continue
+            
+        # Samples above threshold are removed. At the border of the ball tracking system often noisy
+        # samples can be found.
+        if key > len(time_stamps) - index_threshold:
+            keys_to_remove.append(key)
+            continue
+        
+        # Check if there is a sudden time jump which resulted in a false detected ball contact.
+        # There should be no large gap and therefore the sample time stamps before and after should be
+        # smaller the the given time threshold. We assume 200Hz sampling. So 5 samples should be
+        # collected in 0.025 s. 
+        contact_ts = time_stamps[key]
+        contact_ts_lower = time_stamps[key - time_filter_stamp_range]
+        contact_ts_upper = time_stamps[key + time_filter_stamp_range]
+        
+        if contact_ts - contact_ts_lower > time_threshold:
+            keys_to_remove.append(key)
+            continue
+        
+        if contact_ts_upper - contact_ts > time_threshold:
+            keys_to_remove.append(key)
+            continue
+    
+    print(keys_to_remove)
+    for key in keys_to_remove:
+        del filtered_contact_dict[key]
+    
+    return filtered_contact_dict
+
+
+def calculate_rebound_velocity(v_before, racket_normal):
+    # ! Assumptions:
+    # Point-contact of ball and surface, ball is not spinning,
+    # ball does not lose energy due to friction or slip
+    # ball and racket does not lose energy due to elastic deformation of the ball
+    # all energy is fully maintained.
+    # surface is perfectly flat and is fully described by normal vector.
+
+    v_before = array(v_before)
+    racket_normal = array(racket_normal)
+
     # Normalize vectors
-    V_initial = array(V_initial) / norm(V_initial)
-    C_normal = array(C_normal) / norm(C_normal)
+    racket_normal = racket_normal / norm(racket_normal)
 
-    # Convert racket orientation to a unit quaternion
-    R_quaternion = quat.from_float_array(R_orient)
-    R_quaternion = quat.as_quat_array(R_quaternion / norm(R_quaternion))
+    # Calculate the component of v_in that is perpendicular to the surface
+    v_perpendicular = (v_before @ racket_normal) * racket_normal
 
-    # Calculate reflection vector
-    R_reflection = V_initial - 2 * np.dot(V_initial, C_normal) * C_normal
+    # Calculate the component of v_in that is parallel to the surface
+    v_parallel = v_before - v_perpendicular
 
-    # Transform reflection vector using quaternion rotation
-    R_global = quat.as_float_array(
-        R_quaternion * quat.as_quat_array(R_reflection) * quat.conjugate(R_quaternion)
-    )
-
-    # Return the rebound velocity vector
-    return R_global.tolist()
+    # Calculate the velocity vector of the ball after the collision (v_out)
+    return v_parallel - v_perpendicular
 
 
 def check_difference_below_threshold(indices, threshold):
@@ -304,7 +398,6 @@ def estimate(
     positions: Sequence[Sequence[float]],
     velocities: Optional[Sequence[Sequence[float]]] = None,
     racket_orientation: Optional[Sequence[float]] = None,
-    regression: bool = True,
     n_regression_samples: int = 10,
     return_polar: bool = False,
 ):
@@ -324,13 +417,15 @@ def estimate(
 
     # get all rebounds from trajectory and specify if table or racket
     contact_dict = detect_rebounds(time_stamps, positions)
+    contact_dict = filter_rebounds(contact_dict)
+
     rebound_indices = list(contact_dict.keys())
 
     if check_difference_below_threshold(rebound_indices, n_regression_samples):
         logging.warning("Identified rebound to close.")
-    
+
     contact_list = []
-    
+
     # take velocity vector before bounce and after bounce
     for index, contact_type in contact_dict.items():
         if velocities is None:
@@ -357,7 +452,7 @@ def estimate(
             vel_after_bounce = vel_after_bounce[0]
         else:
             velocities = array(velocities, copy=True)
-            
+
             vel_before_bounce = velocities[index - 1]
             vel_after_bounce = velocities[index + 1]
 
@@ -372,7 +467,7 @@ def estimate(
         vel_diff = vel_after_bounce - vel_after_bounce_no_spin
 
         spin_effect = vel_diff
-        
+
         if return_polar:
             v_x = vel_diff[0]
             v_y = vel_diff[1]
@@ -390,5 +485,5 @@ def estimate(
 
         contact_dict[index] = [contact_type, spin_effect]
         contact_list.append((index, contact_type, spin_effect))
-    
+
     return contact_dict
--- examples/spin_estimation_vis.py
+++ examples/spin_estimation_vis.py
@@ -12,6 +12,7 @@ from ball_prediction.spin_estimator import (
     detect_rebounds,
     get_regressed_state,
     step_ball_simulation,
+    filter_rebounds,
 )
 
 FILE_PATH = "/home/lis/workspace/spin_project/workspace/src/ball_prediction/examples/data/ball_trajectories_with_return.hdf5"
@@ -62,6 +63,26 @@ def visualize_data():
     for i in range(3):
         axs[i].scatter(ball_time_stamps, ball_positions[:, i], s=marker_size)
         axs[i + 3].scatter(ball_time_stamps, ball_velocities[:, i], s=marker_size)
+
+    fig, axs = plt.subplots(3)
+    axs[0].scatter(ball_positions[:, 0], ball_positions[:, 1], s=marker_size)
+    axs[1].scatter(ball_positions[:, 1], ball_positions[:, 2], s=marker_size)
+    axs[2].scatter(ball_positions[:, 0], ball_positions[:, 2], s=marker_size)
+
+
+def visualize_data_3d():
+    marker_size = 1.25
+    index = str(INDEX)
+
+    collection = load_data()
+
+    ball_time_stamps = collection[index]["ball_time_stamps"]
+    ball_positions = collection[index]["ball_positions"]
+    ball_velocities = collection[index]["ball_velocities"]
+
+    ax = plt.figure().add_subplot(projection="3d")
+    ax.scatter(ball_positions[:, 0], ball_positions[:, 1], ball_positions[:, 2])
+    ax.set_aspect("equal")
 
 
 def velocity_regression_visualisation():
@@ -132,6 +153,8 @@ def rebound_visualisation():
     contact_dict, info = detect_rebounds(
         time_stamps=ball_time_stamps, positions=ball_positions, return_states=True
     )
+
+    print(contact_dict)
 
     regressed_states = info["ball_state_history"]
     simulated_states = info["simulated_ball_state_history"]
@@ -453,13 +476,13 @@ def load_data():
 
 def test_indicies_compare():
     from ball_prediction.spin_estimator import check_difference_below_threshold
-    
+
     threshold = 5
     n_runs = 100
-    
+
     for i in range(n_runs):
         rebounds_indices = randint(0, 200, (5,))
-        
+
         rebounds_indices = sort(rebounds_indices)
 
         if check_difference_below_threshold(rebounds_indices, threshold=threshold):
@@ -468,7 +491,7 @@ def test_indicies_compare():
 
 def test_spin_estimator():
     from ball_prediction.spin_estimator import estimate
-    
+
     index = str(INDEX)
 
     collection = load_data()
@@ -478,23 +501,375 @@ def test_spin_estimator():
 
     ball_time_stamps = array(ball_time_stamps)
     ball_positions = array(ball_positions)
-    
+
     output = estimate(ball_time_stamps, ball_positions)
-    
+
     print(output)
+
+
+def test_contact_dict_filter():
+    marker_size = 1.75
+    index = str(INDEX)
+
+    collection = load_data()
+
+    ball_time_stamps = collection[index]["ball_time_stamps"]
+    ball_positions = collection[index]["ball_positions"]
+    ball_velocities = collection[index]["ball_velocities"]
+
+    contact_dict, info = detect_rebounds(
+        time_stamps=ball_time_stamps, positions=ball_positions, return_states=True
+    )
     
+    filtered_contact_dict = filter_rebounds(contact_dict, ball_time_stamps, time_threshold=0.05, time_filter_stamp_range=5)
+
+    print(contact_dict)
+    print(filtered_contact_dict)
     
+    regressed_states = info["ball_state_history"]
+    simulated_states = info["simulated_ball_state_history"]
+    distances = info["xy_pred_errors"]
+    distances_table = info["z_pred_errors"]
+
+    regressed_states = array(regressed_states)
+    simulated_states = array(simulated_states)
+
+    # Plot regressed states
+    fig, axs = plt.subplots(7, sharex=True)
+    for i in range(3):
+        axs[i].scatter(
+            ball_time_stamps, ball_positions[:, i], s=marker_size, label="data"
+        )
+        
+        axs[i+3].scatter(
+            ball_time_stamps, ball_positions[:, i], s=marker_size, label="data"
+        )
+
+    for i in range(3):
+        for index, contact_type in contact_dict.items():
+            if contact_type == ContactType.RACKET:
+                racket_color = "#17c7d0"
+                axs[i].axvline(
+                    ball_time_stamps[index],
+                    color=racket_color,
+                    linestyle="-",
+                    alpha=0.5,
+                    label="Racket Contact",
+                )
+
+            if contact_type == ContactType.TABLE:
+                table_color = "#46b361"
+                axs[i].axvline(
+                    ball_time_stamps[index],
+                    color=table_color,
+                    linestyle="-",
+                    alpha=0.5,
+                    label="Table Contact",
+                )
+                
+    for i in range(3,6):
+        for index, contact_type in filtered_contact_dict.items():
+            if contact_type == ContactType.RACKET:
+                racket_color = "#17c7d0"
+                axs[i].axvline(
+                    ball_time_stamps[index],
+                    color=racket_color,
+                    linestyle="-",
+                    alpha=0.5,
+                    label="Racket Contact",
+                )
+
+            if contact_type == ContactType.TABLE:
+                table_color = "#46b361"
+                axs[i].axvline(
+                    ball_time_stamps[index],
+                    color=table_color,
+                    linestyle="-",
+                    alpha=0.5,
+                    label="Table Contact",
+                )
+
+
+    # Distance distribution
+    axs[-1].scatter(
+        ball_time_stamps[WINDOW_SIZE:-SIMULATION_DELAY],
+        distances,
+        alpha=1.0,
+        c="#0193d7",
+        label="pred. error xy",
+        s=marker_size,
+    )
+    axs[-1].scatter(
+        ball_time_stamps[WINDOW_SIZE:-SIMULATION_DELAY],
+        distances_table,
+        alpha=0.5,
+        c="#CF5369",
+        label="pred. error z",
+        s=marker_size,
+    )
+
+def visualize_regressed_velocity_vectors():
+    marker_size = 1.25
+    index = str(INDEX)
     
+    arrow_length = 0.15
+    idx_delta = 2
+
+    collection = load_data()
+
+    ball_time_stamps = collection[index]["ball_time_stamps"]
+    ball_positions = collection[index]["ball_positions"]
+    ball_velocities = collection[index]["ball_velocities"]
+    
+    contact_dict, info = detect_rebounds(
+        time_stamps=ball_time_stamps, positions=ball_positions, return_states=True
+    )
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    
+    ax.scatter(ball_positions[:, 0], ball_positions[:, 1], ball_positions[:, 2])
+    
+    for key, value in contact_dict.items():
+        idx_before = key - idx_delta
+        pos_x, pos_y, pos_z = ball_positions[idx_before]
+        vel_x, vel_y, vel_z = ball_velocities[idx_before]
+        ax.quiver(pos_x, pos_y, pos_z, vel_x, vel_y, vel_z,
+                length=arrow_length, color='red', label=f'Velocity before')
+
+
+        idx_after = key + idx_delta
+        pos_x, pos_y, pos_z = ball_positions[idx_after]
+        vel_x, vel_y, vel_z = ball_velocities[idx_after]
+        ax.quiver(pos_x, pos_y, pos_z, vel_x, vel_y, vel_z,
+                length=arrow_length, color='green', label=f'Velocity after')
+        
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    # Set the aspect ratio of the 3D plot to be equal
+    ax.set_aspect("equal")
+    pass
+
+
+def test_orientation_calculation():
+    import math
+
+    import numpy as np
+    from scipy.spatial.transform import Rotation
+
+    from ball_prediction.spin_estimator import compute_racket_orientation
+
+    np.set_printoptions(suppress=True)
+    pi = math.pi
+
+    joint_angles_rad = [40, 00, 0.0, 0.0]
+    length_upper_arm = 0.5
+    lenght_lower_arm = 0.4
+    degrees = True
+
+    theta_1, theta_2, theta_3, theta_4 = joint_angles_rad
+
+    o = [0.0, 0.0, 0.0]
+
+    o_0 = [0.0, 1.0, 0.0]
+
+    R1 = Rotation.from_rotvec(np.deg2rad([0.0, 0.0, theta_1]))  # Shoulder joint
+    R2 = Rotation.from_rotvec(np.deg2rad([0.0, theta_2, 0.0]))  # Shoulder joint
+
+    R12 = R2 * R1
+    T1 = [0, 0, length_upper_arm]
+    R3 = Rotation.from_rotvec(np.deg2rad([0.0, theta_3, 0.0]))  # Elbow joint
+    T2 = [0, 0, lenght_lower_arm]
+    R4 = Rotation.from_rotvec(np.deg2rad([0.0, 0.0, theta_4]))  # Wrist joint
+
+    o_2 = R12.apply(o_0)
+    o_2 += T1
+
+    o_3 = R3.apply(o_2)
+    o_3 += T2
+
+    o_4 = R4.apply(o_3)
+
+    # Visualisation
+    ax = plt.figure().add_subplot(projection="3d")
+    x_axis = np.array([0.4, 0, 0])
+    y_axis = np.array([0, 0.4, 0])
+    z_axis = np.array([0, 0, 0.4])
+
+    # Draw the basis vectors as arrows
+    ax.quiver(*o, *x_axis, color="r", label="X-axis")
+    ax.quiver(*o, *y_axis, color="g", label="Y-axis")
+    ax.quiver(*o, *z_axis, color="b", label="Z-axis")
+
+    ax.scatter(*o, s=20, c="r", alpha=0.5)
+
+    # Shoulder
+    P0 = o
+    print(f"P0: {P0}")
+    ax.scatter(*P0, s=20, c="b", alpha=0.5)
+
+    # Elbow
+    P1 = o_2
+    print(f"P1: {P1}")
+    ax.scatter(*P1, s=20, c="g", alpha=0.5)
+
+    # Wrist
+    P2 = o_3
+    print(f"P2: {P2}")
+    ax.scatter(*P2, s=20, c="black", alpha=0.5)
+
+    # Shoulder to Elbow
+    P0P1 = [[i, j] for i, j in zip(P0, P1)]
+    print(P0P1)
+    ax.plot(*P0P1)
+
+    # Elbow to Wrist
+    P1P2 = [[i, j] for i, j in zip(P1, P2)]
+    print(P1P2)
+    ax.plot(*P1P2)
+
+    ax.set_xlim(-1, 1)
+    ax.set_ylim(-1, 1)
+    ax.set_zlim(-1, 1)
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.set_zlabel("Z")
+
+    pass
+
+
+def test_orientation_calculation_homogenous():
+    import math
+
+    import numpy as np
+    from scipy.spatial.transform import Rotation
+
+    from ball_prediction.spin_estimator import compute_racket_orientation
+
+    np.set_printoptions(suppress=True)
+    pi = math.pi
+
+    joint_angles_deg = [20, 30, 00, 0]
+    joint_angles_rad = np.deg2rad(joint_angles_deg)
+    length_upper_arm = 0.5
+    length_lower_arm = 0.4
+
+    theta_1, theta_2, theta_3, theta_4 = joint_angles_rad
+
+    o = [0.0, 0.0, 0.0]
+
+    def translation_matrix(x, y, z):
+        return np.array([[1, 0, 0, x], [0, 1, 0, y], [0, 0, 1, z], [0, 0, 0, 1]])
+
+    def rotation_matrix_x(theta):
+        c = np.cos(theta)
+        s = np.sin(theta)
+        return np.array([[1, 0, 0, 0], [0, c, -s, 0], [0, s, c, 0], [0, 0, 0, 1]])
+
+    def rotation_matrix_y(theta):
+        c = np.cos(theta)
+        s = np.sin(theta)
+        return np.array([[c, 0, s, 0], [0, 1, 0, 0], [-s, 0, c, 0], [0, 0, 0, 1]])
+
+    def rotation_matrix_z(theta):
+        c = np.cos(theta)
+        s = np.sin(theta)
+        return np.array([[c, -s, 0, 0], [s, c, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+
+    print(theta_1)
+    print(theta_2)
+    print(theta_3)
+    print(theta_4)
+
+    T_10 = rotation_matrix_z(theta_1)
+    T_21 = rotation_matrix_y(theta_2)
+    T_32 = translation_matrix(0, 0, length_upper_arm)
+
+    T_43 = rotation_matrix_y(theta_3)
+    T_54 = translation_matrix(0, 0, length_lower_arm)
+    T_65 = rotation_matrix_z(theta_4)
+
+    origin = np.array([0, 0, 0])
+
+    homogeneous_coordinate = np.append(origin, 1)
+
+    T_20 = T_21 @ T_10
+    T_30 = T_32 @ T_21 @ T_10
+    T_40 = T_43 @ T_32 @ T_21 @ T_10
+    T_50 = T_54 @ T_43 @ T_32 @ T_21 @ T_10
+    T_60 = T_65 @ T_54 @ T_43 @ T_32 @ T_21 @ T_10
+
+    end_point_00 = homogeneous_coordinate  # origin
+    end_point_10 = T_10 @ end_point_00
+    end_point_20 = T_21 @ end_point_10
+    end_point_30 = T_32 @ end_point_20  # joint 1
+    end_point_40 = T_43 @ end_point_30
+    end_point_50 = T_54 @ end_point_40  # joint 2
+    end_point_60 = T_65 @ end_point_50
+
+    # Plotting the 3D visualization
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+
+    # Plot the joints
+    joints = [end_point_00[:3], end_point_30[:3], end_point_50[:3]]
+    for i, joint in enumerate(joints):
+        ax.scatter(joint[0], joint[1], joint[2], c="r", marker="o", s=36)
+        ax.text(
+            joint[0],
+            joint[1],
+            joint[2],
+            f"Joint {i}",
+            fontsize=12,
+            ha="right",
+            va="bottom",
+        )
+
+    # Set axis labels (optional)
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.set_zlabel("Z")
+
+    # Set the aspect ratio of the 3D plot to be equal
+    ax.set_aspect("equal")
+
+    # Add a legend for the coordinate system axes
+    ax.legend()
+
+
+def visualize_velocity_vectors():
+    marker_size = 1.25
+    index = str(INDEX)
+
+    collection = load_data()
+
+    ball_time_stamps = collection[index]["ball_time_stamps"]
+    ball_positions = collection[index]["ball_positions"]
+    ball_velocities = collection[index]["ball_velocities"]
+
+    ax = plt.figure().add_subplot(projection="3d")
+    ax.scatter(ball_positions[:, 0], ball_positions[:, 1], ball_positions[:, 2])
+    ax.set_aspect("equal")
+
 
 if __name__ == "__main__":
     # visualize_data()
-    velocity_regression_visualisation()
-    rebound_visualisation()
+    # visualize_data_3d()
+    # velocity_regression_visualisation()
+    # rebound_visualisation()
     # test_ball_simulation()
-    compare_poly_degrees_visualisation()
-    compare_windows_visualisation()
-    
+    # compare_poly_degrees_visualisation()
+    # compare_windows_visualisation()
+
+    # test_orientation_calculation()
+    # test_orientation_calculation_homogenous()
+
     # test_indicies_compare()
     # test_spin_estimator()
 
+    # test_contact_dict_filter()
+    
+    visualize_regressed_velocity_vectors()
+    
     plt.show()
