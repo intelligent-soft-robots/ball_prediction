f22988dfacdfd5e09de3541443874cccfeaa16e7
Alexander Dittrich <alexander@dittrich.xyz>
2023-09-03 11:31:02 +0200

    data filtering

--- ball_prediction/utils/data_filter.py
+++ ball_prediction/utils/data_filter.py
@@ -1,30 +1,151 @@
-from typing import Tuple
+from typing import Any, Dict, List, Tuple
 
 import numpy as np
 
 
-def filter_time_span(item, min_t, max_t):
-    time_stamps = item["ball_time_stamps"]
+def filter_time_span(data: dict, min_t: float, max_t: float) -> List[Dict[str, Any]]:
+    new_data = data.copy()
+
+    general_keys = ["launch_parameter", "date_stamp", "time_stamps"]
+
+    ball_keys = [
+        "ball_ids",
+        "ball_time_stamps",
+        "ball_positions",
+        "ball_velocities",
+    ]
+
+    robot_keys = [
+        "robot_time_stamps",
+        "robot_joint_angles",
+        "robot_joint_angles_desired",
+        "robot_joint_angle_velocities",
+        "robot_joint_angle_velocities_desired",
+        "robot_pressures_agonist",
+        "robot_pressures_antagonist",
+    ]
+
+    # filter ball data
+    ball_time_stamps = data["ball_time_stamps"]
 
     indices = []
-    remove_indices = np.nonzero(min_t > time_stamps)[0]
+
+    remove_indices = np.nonzero(min_t > ball_time_stamps)[0]
     indices.extend(remove_indices)
 
-    remove_indices = np.nonzero(time_stamps > max_t)[0]
+    remove_indices = np.nonzero(ball_time_stamps > max_t)[0]
     indices.extend(remove_indices)
 
-    return indices
+    for key in ball_keys:
+        val = new_data[key]
+        filtered_val = np.delete(val, indices, axis=0)
+        new_data[key] = filtered_val
+
+    # filter robot data
+    robot_time_stamps = data["robot_time_stamps"]
+
+    indices = []
+
+    remove_indices = np.nonzero(min_t > robot_time_stamps)[0]
+    indices.extend(remove_indices)
+
+    remove_indices = np.nonzero(robot_time_stamps > max_t)[0]
+    indices.extend(remove_indices)
+
+    for key in robot_keys:
+        val = new_data[key]
+        filtered_val = np.delete(val, indices, axis=0)
+        new_data[key] = filtered_val
+
+    return new_data
+
+
+def remove_empty_data(collection: list):
+    new_collection = []
+
+    def is_empty(value):
+        if isinstance(value, list) or isinstance(value, np.ndarray):
+            return len(value) == 0
+        return False
+
+    for data in collection:
+        add_data = True
+        has_empty_entry = any(is_empty(value) for value in data.values())
+
+        if has_empty_entry:
+            add_data = False
+            print("empty!")
+
+        if add_data:
+            new_collection.append(data)
+
+    return new_collection
+
+
+def remove_sparse_data(
+    collection: list,
+    min_num_samples: int = 100,
+    max_num_samples: int = 700,
+    verbose: bool = True,
+):
+    new_collection = []
+
+    big_counter = 0
+    small_counter = 0
+
+    for data in collection:
+        if len(data["ball_time_stamps"]) < min_num_samples:
+            small_counter += 1
+            continue
+
+        if len(data["ball_time_stamps"]) > max_num_samples:
+            big_counter += 1
+            continue
+
+        new_collection.append(data)
+
+    if verbose:
+        print(f"{small_counter} small trajectories removed.")
+        print(f"{big_counter} many trajectories removed.")
+
+    return new_collection
+
+
+def remove_patchy_ball_data(
+    collection: list, max_patch_time: float, verbose: bool = True
+):
+    new_collection = []
+
+    gap_counter = 0
+
+    for data in collection:
+        time_gaps = np.diff(data["ball_time_stamps"])
+        largest_gap = max(time_gaps)
+
+        if len(time_gaps) == 0:
+            print(f"Ball data is empty!")
+
+        if largest_gap > max_patch_time:
+            gap_counter += 1
+            continue
+
+        new_collection.append(data)
+
+    if verbose:
+        print(f"{gap_counter} patchy data removed.")
+
+    return new_collection
 
 
 def filter_outside_region(
-    item,
+    data,
     xlimit: Tuple[float],
     ylimit: Tuple[float],
     zlimit: Tuple[float],
 ):
     limits = [xlimit, ylimit, zlimit]
 
-    positions = item["ball_positions"]
+    positions = data["ball_positions"]
     delete_indices = []
 
     for idx in range(len(positions)):
@@ -37,7 +158,7 @@ def filter_outside_region(
 
 
 def remove_samples(item, indices_to_remove):
-    print(indices_to_remove)
+    filtered_item = item.copy()
 
     for key, values in item.items():
         if key == "launch_parameter":
@@ -46,6 +167,15 @@ def remove_samples(item, indices_to_remove):
         if key == "date_stamp":
             continue
 
-        item = np.delete(values, indices_to_remove, axis=0)
+        print(f"Before deletion: key='{key}', values.shape={values.shape}")
+        try:
+            values = np.delete(values, indices_to_remove, axis=0)
+        except IndexError as e:
+            print(f"Error: {e}")
+            print(indices_to_remove)
 
-    return item
+        print(f"After deletion: key='{key}', values.shape={values.shape}")
+        print("#################################")
+        filtered_item[key] = values
+
+    return filtered_item
--- examples/generate_data_set.py
+++ examples/generate_data_set.py
@@ -7,51 +7,72 @@ import numpy as np
 from ball_prediction.utils.data_filter import (
     filter_outside_region,
     filter_time_span,
+    remove_empty_data,
+    remove_patchy_ball_data,
     remove_samples,
+    remove_sparse_data,
 )
 from ball_prediction.utils.data_management import load_robot_ball_data
 
 FILE_PATH = "/home/lis/workspace/spin_project/workspace/src/ball_prediction/data/no_spin_robot.hdf5"
 
 
-# load dataset
-def loading():
-    marker_size = 1.25
-
+def load_dataset():
     file_path = FILE_PATH
-
     collection = load_robot_ball_data(file_path)
+    return collection
 
-    ax = plt.figure().add_subplot(projection="3d")
-    counter = 0
 
+# load dataset
+def filter_dataset():
+    _collection = load_dataset()
+    print(f"Raw data: {len(_collection)}")
+
+    # Data Length Filter
+    _collection = remove_sparse_data(_collection)
+
+    # Time Span Filter
     min_time_step = 3.0
     max_time_step = 4.5
 
-    x_lims = (-3.0, 3.0)
-    y_lims = (-3.0, 3.0)
-    z_lims = (-1.4, 2.0)
+    new_collection = []
+    for data in _collection:
+        data = filter_time_span(data, min_time_step, max_time_step)
+        new_collection.append(data)
+    _collection = new_collection
+    print(f"Time span filtered data: {len(_collection)}")
 
+    # Region Filter
+    xlimit = (-3.0, 3.0)
+    ylimit = (-3.0, 1.0)
+    zlimit = (-1.4, 2.0)
+
+    new_collection = []
+    for data in _collection:
+        data = filter_outside_region(data, xlimit, ylimit, zlimit)
+        new_collection.append(data)
+    _collection = new_collection
+    print(f"Region filtered data: {len(_collection)}")
+
+    # Patchy Filter
+    _collection = remove_patchy_ball_data(_collection, 0.2)
+    print(f"Patch filtered data: {len(_collection)}")
+
+    # Visualize
+    plot_ball_data(_collection)
+
+
+def plot_ball_data(collection: list):
+    ax = plt.figure().add_subplot(projection="3d")
     fig, axs = plt.subplots(3)
 
-    for key, item in collection.items():
-        ball_time_stamps = item["ball_time_stamps"]
-        ball_positions = item["ball_positions"]
-        ball_velocities = item["ball_velocities"]
+    counter = 0
+    for data in collection:
+        ball_time_stamps = data["ball_time_stamps"]
+        ball_positions = data["ball_positions"]
 
         time_stamps = np.array(ball_time_stamps)
         positions = np.array(ball_positions)
-        velocities = np.array(ball_velocities)
-
-        removal_indices = []
-
-        indices = filter_time_span(item, min_time_step, max_time_step)
-        removal_indices.extend(indices)
-
-        # indices = filter_outside_region(item, x_lims, y_lims, z_lims)
-        # removal_indices.extend(indices)
-
-        item = remove_samples(item, removal_indices)
 
         try:
             ax.plot(positions[:, 0], positions[:, 1], positions[:, 2])
@@ -59,10 +80,43 @@ def loading():
             for i in range(3):
                 axs[i].plot(time_stamps, positions[:, i])
 
-        except:
+        except Exception as e:
             counter += 1
+            print(data["ball_time_stamps"])
 
-    print(counter)
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.set_zlabel("Z")
+
+    ax.set_aspect("equal")
+
+    print(f"{counter} data not plotted.")
+
+
+def plot_all_data(collection):
+    fig, axs = plt.subplots(len(data))
+    ax = plt.figure().add_subplot(projection="3d")
+    counter = 0
+
+    data = collection[0]
+
+    for i, (key, val) in enumerate(data.items()):
+        if key == "launch_parameter":
+            print(val)
+
+        if key == "time_stamps":
+            print(val)
+
+        if key == "ball_time_stamps":
+            print(val)
+
+        if key == "robot_time_stamps":
+            print(val)
+
+        axs[i].plot(val)
+        axs[i].set_ylabel(key)
+
+    plt.show()
 
 
 def export_dataset():
@@ -71,10 +125,11 @@ def export_dataset():
 
 
 def split_dataset():
+    # store dataset
     ...
 
 
 if __name__ == "__main__":
-    loading()
+    filter_dataset()
 
     plt.show()
